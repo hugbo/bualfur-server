c3404b6d4df5aad97dc4568c0e833d1eadcd1adf
Stefan Carl Peiser <scg2@hi.is>
2016-11-04 21:00:03 +0000

    updated frontend search

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,30 +1,40 @@
-$(function(){
-  console.log("ready!");
-})
+$(function() {
+    console.log("ready!");
+
+    $('#search-button').click(function() {
+        console.log("Ayyy lmao");
+
+        var valuesToSubmit = {
+          zipcode: $('#zipcode').val(),
+          priceMin: $('#priceMin').val(),
+          priceMax: $('#priceMax').val(),
+          roomsMin: $('#roomsMin').val(),
+          roomsMax: $('#roomsMax').val(),
+          propertyType: $('#propertyType').val()
+        }
+
+        console.log(valuesToSubmit);
+        $.ajax({
+            type: "POST", url: "/properties/getjson.json", //sumbits it to the given url of the form
+            data: {
+                search: valuesToSubmit
+            },
+            dataType: "JSON" // you want a difference between normal and ajax-calls, and json is standard
+        }).success(function(json) {
+            console.log("success", json);
+        });
+        return false; // prevents normal behaviour
+    });
+});
 
 function initMap() {
-  var uluru = {lng: -21.949227, lat: 64.140457};
-  var map = new google.maps.Map(document.getElementById('map'), {
+var uluru = {
+    lng: -21.949227,
+    lat: 64.140457
+};
+var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: uluru
-  });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
-
-
-  $('form').submit(function() {
-      var valuesToSubmit = "3000";
-      // $(this).serialize();
-      $.ajax({
-          type: "POST",
-          url: "/properties/getjson.json", //sumbits it to the given url of the form
-          data: {search: valuesToSubmit},
-          dataType: "JSON" // you want a difference between normal and ajax-calls, and json is standard
-      }).success(function(json){
-          console.log("success", json);
-      });
-      return false; // prevents normal behaviour
-  });
+});
+var marker = new google.maps.Marker({position: uluru, map: map});
 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102215945) do
+ActiveRecord::Schema.define(version: 20161104133612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 20161102215945) do
     t.string   "propertyType"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
 end
